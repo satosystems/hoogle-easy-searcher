415640d1291ad505136a37bc4da13c6562ee29c1
Satoshi Ogata <satosystems@gmail.com>
2019-04-15 12:17:39 +0900

    refactor
    以下のリファクタリングを行った。
    
    - ESLint standard に従いコードを修正
    - クエリフィールドにフォーカスが当たるようにした
    - パッケージ化スクリプトを実装
    - 末尾改行の重複を削除
    - その他細かい修正

--- popup.js
+++ popup.js
@@ -1,22 +1,17 @@
 window.onload = () => {
-  const lts = document.getElementById("lts");
-  const query = document.getElementById("query");
+  const query = document.getElementById('query')
+  const lts = document.getElementById('lts')
   const onKeyPress = event => {
     if (event.keyCode === 13) {
-      let url;
-      if (lts.value === "") {
-        url = "https://www.haskell.org/hoogle/?hoogle=" + query.value;
-      } else {
-        url = "https://www.stackage.org/" + lts.value + "/hoogle?q=" + query.value;
-      }
-      document.cookie = "lts=" + encodeURIComponent(lts.value);
-      chrome.tabs.create({"url": url});
+      const url = lts.value === '' ? `https://www.haskell.org/hoogle/?hoogle=${query.value}` : `https://www.stackage.org/${lts.value}/hoogle?q=${query.value}`
+      document.cookie = `lts=${encodeURIComponent(lts.value)}`
+      chrome.tabs.create({'url': url}) // eslint-disable-line
     }
-  };
-  if (document.cookie) {
-    lts.value = document.cookie.split(";")[0].split("=")[1];
   }
-  lts.addEventListener("keypress", onKeyPress);
-  query.addEventListener("keypress", onKeyPress);
+  if (document.cookie) {
+    lts.value = document.cookie.split(';')[0].split('=')[1]
+  }
+  query.addEventListener('keypress', onKeyPress)
+  lts.addEventListener('keypress', onKeyPress)
+  query.focus()
 }
-
